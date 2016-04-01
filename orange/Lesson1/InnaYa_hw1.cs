9dfa6dd5a7c2758510782c50986224042a4c2556
applesinn4ik <applesinn4ik@gmail.com>
2016-04-01 12:55:58 +0300

    Adding HW1

---
+++ orange/Lesson1/InnaYa_hw1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnaYa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            do
+            {
+                Console.WriteLine("Hello, my dear friend. Enter the first number");
+                string value1 = Console.ReadLine();
+                x = Convert.ToInt32(value1);
+
+                Console.WriteLine("Thanks, now enter the second number");
+                string value2 = Console.ReadLine();
+                y = Convert.ToInt32(value2);
+                z = x % y;
+
+                if (z == 0) Console.WriteLine("Well done, take a candy!");
+                else Console.WriteLine("Try again, no candies today :(");
+            }
+            while (z != 0);
+
+            Console.ReadLine();
+        }
+    }
+}
