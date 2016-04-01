5b10258e8f9b36c045e512936555bfea8b543ea6
applesinn4ik <applesinn4ik@gmail.com>
2016-04-01 13:16:16 +0300

    Adding hw2

---
+++ orange/Lesson2/InnaYa_hw2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            for (int a = 1; a <= 100; a++)
+            {
+                bool fizz = a % 3 == 0;
+                bool buzz = a % 5 == 0;
+                System.Threading.Thread.Sleep(1000);
+                if (fizz && buzz) Console.WriteLine("FizzBuzz");
+                    
+                else if (fizz) Console.WriteLine("Fizz");
+
+                else if (buzz) Console.WriteLine("Buzz");
+
+                else Console.WriteLine(a);
+            }
+        }
+    }
+}
